67fd4dbe809775dfeba407d09ba87e567ab5c5b1
Abdelrahman <obada.semary@gmail.com>
2020-05-26 16:30:33 +0300

    Apply MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Abdelrahman Mohamed on 26.05.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+//func createCardContent(pairIndex: Int) -> String {
+//
+//    return "😂"
+//}
+
+class EmojiMemoryGame {
+
+    private(set) var model: MemoryGame<String> = createMemoryGame()
+
+    static func createMemoryGame() -> MemoryGame<String> {
+
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+
+    // MARK: - Access to the Model
+
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+
+    // MARK: - Intents(s)
+
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
